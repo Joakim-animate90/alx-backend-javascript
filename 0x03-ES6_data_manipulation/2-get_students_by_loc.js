9b0fcf1aa4268b68b6f83340223f11af61a94cde
Joakim-Animate90 <joakimbwire23@gmail.com>
2023-01-03 23:27:22 +0300

    Add task 2

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+/* eslint-disable default-param-last */
+import getListStudents from './0-get_list_students';
+
+export default function getStudentsByLocation(student = getListStudents(), city) {
+  return student.filter((el) => el.location === city);
+}
